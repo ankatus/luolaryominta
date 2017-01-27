aa6cc66970f8e8cab42c32e49c2d8fb34d64ce1f
ankatus <antti.j.kataja@gmail.com>
2017-01-27 16:44:31 +0200

    poistettu tyhjä metodi, lisätty kenttä

--- luolaryominta/src/main/java/fi/ana/logic/Game.java
+++ luolaryominta/src/main/java/fi/ana/logic/Game.java
@@ -1,18 +1,18 @@
 
 package fi.ana.logic;
 
+import java.util.*;
 
 public class Game {
     
+    private List<Character> monsters;
+    private PlayerCharacter player;
     private Map map;
-    
+    private MapMaker mapMaker;
+     
     public Game() {
-        
+        mapMaker = new MapMaker();
+        map = mapMaker.makeDefaultMap();
     }
-    
-    public void nextTurn() {
-        
-    }
-    
-    
+     
 }
