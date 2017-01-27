e1cb622e986e06ff4171797b56397187aaa08095
ankatus <antti.j.kataja@gmail.com>
2017-01-27 16:38:23 +0200

    MapMaker-luokka luotu, sisältää toiminnallisuuden testikartan tekemiseksi

---
+++ luolaryominta/src/main/java/fi/ana/logic/MapMaker.java
@@ -0,0 +1,32 @@
+
+package fi.ana.logic;
+
+import java.util.*;
+public class MapMaker {
+    
+    
+    public MapMaker() {
+    }
+    
+    public Map makeDefaultMap() {
+        List<Integer> l = new ArrayList<>();
+        Map map = new Map(10,l);
+        populateRow(map, 3, 2, 7, 1);
+        populateColumn(map, 2, 2, 7, 1);
+        return map;
+    }
+    
+    private void populateRow(Map map, int row, int start, int end, int value) {
+        for (int i = start; i <= end; i++) {
+            map.setValue(i, row, value);
+        }
+    }
+    
+    private void populateColumn(Map map, int column, int top, int bottom, int value) {
+        for (int i = top; i <= bottom; i++) {
+            map.setValue(column, i, value);
+        }
+    }
+    
+    
+}
