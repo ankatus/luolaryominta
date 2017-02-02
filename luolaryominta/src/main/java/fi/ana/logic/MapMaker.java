c47decf78e533364fb887262950b32b7fb3984e1
ankatus <antti.j.kataja@gmail.com>
2017-02-02 08:27:53 +0200

    arvojen twiikkausta

--- luolaryominta/src/main/java/fi/ana/logic/MapMaker.java
+++ luolaryominta/src/main/java/fi/ana/logic/MapMaker.java
@@ -1,34 +1,34 @@
-
 package fi.ana.logic;
 
 import java.util.*;
+
 public class MapMaker {
-    
-    
+
     public MapMaker() {
     }
-    
-    public Map makeDefaultMap() {
+
+    public GameMap makeDefaultMap() {
         List<Integer> l = new ArrayList<>();
         l.add(0);
         l.add(1);
-        Map map = new Map(10, l);
+        l.add(2);
+        l.add(3);
+        GameMap map = new GameMap(10, l);
         populateRow(map, 3, 2, 7, 1);
-        populateColumn(map, 2, 2, 7, 1);
+        populateColumn(map, 2, 3, 7, 1);
         return map;
     }
-    
-    private void populateRow(Map map, int row, int start, int end, int value) {
+
+    private void populateRow(GameMap map, int row, int start, int end, int value) {
         for (int i = start; i <= end; i++) {
             map.setValue(i, row, value);
         }
     }
-    
-    private void populateColumn(Map map, int column, int top, int bottom, int value) {
+
+    private void populateColumn(GameMap map, int column, int top, int bottom, int value) {
         for (int i = top; i <= bottom; i++) {
             map.setValue(column, i, value);
         }
     }
-    
-    
+
 }
