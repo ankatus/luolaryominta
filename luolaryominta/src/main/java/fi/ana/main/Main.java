d00ff572c36802c905d2c94758a2629c76d1101b
ankatus <antti.j.kataja@gmail.com>
2017-02-02 08:28:30 +0200

    käynnistää käyttöliittymän

--- luolaryominta/src/main/java/fi/ana/main/Main.java
+++ luolaryominta/src/main/java/fi/ana/main/Main.java
@@ -1,10 +1,12 @@
 
 package fi.ana.main;
-
+import fi.ana.gui.*;
 public class Main {
 
     public static void main(String[] args) {
         // TODO code application logic here
+        TextUI ui = new TextUI();
+        ui.run();
     }
     
 }
